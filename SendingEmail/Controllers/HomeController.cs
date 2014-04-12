6641ff9ab28b18c972567f783f5bdd3ea602e304
pauloortins <paulo.ortins@gmail.com>
2014-04-12 20:12:13 -0300

    Committing the code

---
+++ SendingEmail/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using SendingEmail.Models;
+
+namespace SendingEmail.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendEmail(EmailModel email)
+        {
+            /*
+             * This config, when in a production code should be placed in the app config, in this way,
+             * you can change the config without have to change and recompile the code
+             */
+
+            const string fromEmail = "Your email's address";
+            const string fromPassword = "Your password";            
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail, fromPassword)
+            };
+
+            var message = new MailMessage(
+                from: fromEmail,
+                to: email.To,
+                subject: email.Subject,
+                body: email.Body);
+            
+            smtp.Send(message);            
+
+            return View("Index");
+        }
+
+    }
+}
---
+++ SendingEmail/Models/EmailModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendingEmail.Models
+{
+    public class EmailModel
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
